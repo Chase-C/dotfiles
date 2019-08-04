d6fa5cf0e16be8aa9ec2c3f8870205de7a8cbd33
Chase Cummings <chaseecummings@gmail.com>
2019-08-03 23:36:45 -0700

    Add fish config file

---
+++ .config/fish/config.fish
@@ -0,0 +1,27 @@
+abbr -a q exit
+abbr -a e nvim
+
+if command -v exa > /dev/null
+    abbr -a l exa
+    abbr -a ls exa
+    abbr -a ll exa -l
+    abbr -a la exa -la
+else
+    abbr -a l ls
+    abbr -a ll ls -l
+    abbr -a la ls -la
+end
+
+function fish_prompt
+    echo -n (whoami)
+    echo -n ' | '
+    if [ $PWD != $HOME ]
+        echo -n (basename $PWD)
+    else
+        echo -n '~'
+    end
+    echo ' ▶ '
+end
+
+function fish_greeting
+end
