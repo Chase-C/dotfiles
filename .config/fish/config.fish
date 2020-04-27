2d703dbe8202666ede9b3a09d3488757ad2b8747
Chase Cummings <Chase.Cummings@poly.com>
2020-04-27 08:49:42 -0700

    Modify for work laptop

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -12,8 +12,9 @@ abbr -a aur pacaur --color=auto
 set -U fish_user_paths $fish_user_paths ~/bin
 
 # Set environment variables
-set -g -x MOZ_ENABLE_WAYLAND 1
 set -g -x EDITOR nvim
+set -g -x MOZ_ENABLE_WAYLAND 1
+set -g -x NOTMUCH_CONFIG ~/.config/notmuch/config
 set -g -x ECS_CLUSTER_PREFIX '^'
 set -g -x _JAVA_AWT_WM_NONREPARENTING 1
 
