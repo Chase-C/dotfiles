b942a47e5201d04236029f450d94ff315a2802cb
Chase Cummings <Chase.Cummings@poly.com>
2023-06-28 11:11:29 -0700

    Add typescript debug functionality to nvim config

--- .config/nvim/lua/plugins/cmp.lua
+++ .config/nvim/lua/plugins/cmp.lua
@@ -31,11 +31,6 @@ return {
         winhighlight = 'Normal:Normal,FloatBorder:FloatBorder,CursorLine:Visual,Search:None',
       }
 
-      local function has_words_before()
-        local line, col = unpack(vim.api.nvim_win_get_cursor(0))
-        return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match '%s' == nil
-      end
-
       return {
         enabled = function()
           local dap_prompt = vim.tbl_contains(
@@ -158,7 +153,7 @@ return {
         }
       },
     })
-    
+
     -- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
     cmp.setup.cmdline(':', {
       mapping = cmp.mapping.preset.cmdline(),
--- .config/nvim/lua/plugins/dap.lua
+++ .config/nvim/lua/plugins/dap.lua
@@ -1,28 +1,62 @@
 return {
-  'mfussenegger/nvim-dap',
-  event = 'User SushiFile',
-  dependencies = {
-    {
-      'rcarriga/nvim-dap-ui',
-      opts = { floating = { border = 'rounded' } },
-      config = function(_, opts)
-        local dap, dapui = require('dap'), require('dapui')
-        dap.listeners.after.event_initialized['dapui_config'] = function() dapui.open() end
-        dap.listeners.before.event_terminated['dapui_config'] = function() dapui.close() end
-        dap.listeners.before.event_exited['dapui_config'] = function() dapui.close() end
-        dapui.setup(opts)
-      end,
+  {
+    'mfussenegger/nvim-dap',
+    event = 'User SushiFile',
+  },
+  {
+    'rcarriga/nvim-dap-ui',
+    event = 'User SushiFile',
+    opts = { floating = { border = 'rounded' } },
+    config = function(_, opts)
+      local dap, dapui = require('dap'), require('dapui')
+      dap.listeners.after.event_initialized['dapui_config'] = function() dapui.open() end
+      dap.listeners.before.event_terminated['dapui_config'] = function() dapui.close() end
+      dap.listeners.before.event_exited['dapui_config'] = function() dapui.close() end
+      dapui.setup(opts)
+    end,
+  },
+  {
+    'rcarriga/cmp-dap',
+    event = 'User SushiFile',
+    dependencies = { 'nvim-cmp' },
+    config = function()
+      require('cmp').setup.filetype({ 'dap-repl', 'dapui_watches', 'dapui_hover' }, {
+        sources = {
+          { name = 'dap' },
+        },
+      })
+    end,
+  },
+  {
+    'mxsdev/nvim-dap-vscode-js',
+    event = 'User SushiFile',
+    dependencies = {
+      'microsoft/vscode-js-debug',
+      build = 'npm install --legacy-peer-deps && npx gulp vsDebugServerBundle && mv dist out' 
     },
-    {
-      'rcarriga/cmp-dap',
-      dependencies = { 'nvim-cmp' },
-      config = function()
-        require('cmp').setup.filetype({ 'dap-repl', 'dapui_watches', 'dapui_hover' }, {
-          sources = {
-            { name = 'dap' },
-          },
-        })
-      end,
-    },
+    config = function()
+      require('dap-vscode-js').setup({
+        debugger_path = vim.fn.stdpath('data') .. '/lazy/vscode-js-debug',
+        adapters = { 'pwa-node' },
+      })
+
+      require('dap').configurations['typescript'] = {
+        {
+          type = 'pwa-node',
+          request = 'launch',
+          name = 'Launch file',
+          runtimeExecutable = 'ts-node',
+          program = '${file}',
+          cwd = '${workspaceFolder}',
+        },
+        {
+          type = 'pwa-node',
+          request = 'attach',
+          name = 'Attach',
+          processId = require('dap.utils').pick_process,
+          cwd = '${workspaceFolder}',
+        },
+      }
+    end,
   },
 }
